97bedb9af42f0c113f7ebcac17dfc3ffc0c6072f
Abdullah Alabbad <a.a.al3bad@gmail.com>
2021-11-03 02:08:21 +1100

    ZSH: execute starship command only if it exists

--- dot_config/zsh/plugins/powerlevel10k.plugin.zsh
+++ dot_config/zsh/plugins/powerlevel10k.plugin.zsh
@@ -1,7 +1,14 @@
 if [[ -d ${ZSH}/plugins/powerlevel10k/powerlevel10k ]]; then
-    eval "$(starship init zsh)"
+    # Run this only if starship tool exists
+    if command -v starship &> /dev/null
+    then
+        eval "$(starship init zsh)"
+    fi
+    
+    # Execute the plugin
     source ${ZSH}/plugins/powerlevel10k/powerlevel10k/powerlevel10k.zsh-theme
-
+    
+    # Apply the configuration
     # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
     [[ ! -f ${ZSH}/plugins/powerlevel10k/.p10k.zsh ]] || source ${ZSH}/plugins/powerlevel10k/.p10k.zsh
 else

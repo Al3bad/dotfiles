d87712980e7c9785b13cbe2f43768b31ea2d08b2
Abdullah Alabbad <a.a.al3bad@gmail.com>
2021-11-21 19:50:00 +1100

    TMUX: style status bar

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -5,8 +5,10 @@
 alias weather='curl v2.wttr.in/Melbourne'	# print current weather in Melbourne
 alias mypubip="curl ifconfig.co"
 alias mylocip="get_my_local_ip"
+# alias identify-keycode="identify_keycode"
 
-function get_my_local_ip() { ip route get 1.2.3.4 | awk '{print $7}' }
+function mlocip() { ip route get 1.2.3.4 | awk '{print $7}' }
+function identify-keycode() { xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }' }
 
 #################################################
 # --> lsd tool

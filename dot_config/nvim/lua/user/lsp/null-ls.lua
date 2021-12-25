23e6c0b37a034fea597e3461394bb983edfb3990
Abdullah Alabbad <a.a.al3bad@gmail.com>
2021-12-26 03:23:24 +1100

    NVIM: setup formatter

---
+++ dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -0,0 +1,18 @@
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+	return
+end
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettierd,
+  },
+  -- auto format
+  on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+            vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+        end
+    end,
+})

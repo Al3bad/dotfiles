ab30adebfc42f45a1cac1b34ce61bf16ed94b840
Abdullah Alabbad <a.a.al3bad@gmail.com>
2022-02-21 21:41:15 +1100

    ZSH: setup variables for the history and add aliases for 'ls' command

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -5,10 +5,22 @@
 alias weather='curl v2.wttr.in/Melbourne'	# print current weather in Melbourne
 alias mypubip="curl ifconfig.co"
 alias mylocip="get_my_local_ip"
-# alias identify-keycode="identify_keycode"
 
 function mlocip() { ip route get 1.2.3.4 | awk '{print $7}' }
-function identify-keycode() { xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }' }
+
+#################################################
+# --> ls aliases
+#################################################
+alias ls="ls --color"
+alias ll="ls -al"
+
+#################################################
+# --> xev tool
+#################################################
+if command -v xev &> /dev/null
+then
+  function identify_keycode() { xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }' }
+fi
 
 #################################################
 # --> lsd tool

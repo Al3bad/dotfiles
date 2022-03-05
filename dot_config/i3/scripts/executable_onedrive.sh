483d3a0e182a60f6ceb4761147c0879157cd2afa
Abdullah Alabbad <a.a.al3bad@gmail.com>
2022-03-05 21:56:57 +1100

    I3WM: wait for connection before running onedrive command

--- dot_config/i3/scripts/executable_onedrive.sh
+++ dot_config/i3/scripts/executable_onedrive.sh
@@ -2,6 +2,12 @@
 
 LOG_DIR=/var/log/scripts-log/onedrive_sync.log
 
+# Wait for internet connection
+while ! ip route | grep -oP "default via .+ dev wlp1s0" &> /dev/null ; do
+  sleep 3;
+done
+
+
 onedrive --synchronize 1> $LOG_DIR && {
   # command executed successfully
   notify-send --app-name="OneDrive" " " "Successfully synced with the cloud\!"

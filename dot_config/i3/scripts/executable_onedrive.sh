5305000b560acb1821fe67d56bbe858a74861aea
Abdullah Alabbad <a.a.al3bad@gmail.com>
2022-04-14 22:22:44 +1000

    I3WM: major refactor and update

--- dot_config/i3/scripts/executable_onedrive.sh
+++ dot_config/i3/scripts/executable_onedrive.sh
@@ -10,8 +10,8 @@ done
 
 onedrive --synchronize 1> $LOG_DIR && {
   # command executed successfully
-  notify-send --app-name="OneDrive" " " "Successfully synced with the cloud\!"
+  notify-send --app-name="OneDrive" "Successfully synced with the cloud\!"
 } || {
   # error
-  notify-send --app-name="OneDrive" " " "Failed to sync with the cloud. See log in this directory ${LOG_DIR}"
+  notify-send --app-name="OneDrive" "Failed to sync with the cloud. See log in this directory ${LOG_DIR}"
 }
